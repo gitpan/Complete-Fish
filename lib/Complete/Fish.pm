a35cfbb7085f1a67b2037838c7acf7f7f024403e
Steven Haryanto <stevenharyanto@gmail.com>
2014-07-02 08:32:51 +0000

    Import of SHARYANTO/Complete-Fish-0.00 from CPAN.
    gitpan-cpan-distribution: Complete-Fish
    gitpan-cpan-version:      0.00
    gitpan-cpan-path:         SHARYANTO/Complete-Fish-0.00.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Complete/Fish.pm
@@ -0,0 +1,61 @@
+package Complete::Fish;
+
+use 5.010001;
+
+our $DATE = '2014-07-02'; # DATE
+our $VERSION = '0.00'; # VERSION
+
+1;
+#ABSTRACT: Completion module for Fish shell
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Complete::Fish - Completion module for Fish shell
+
+=head1 VERSION
+
+This document describes version 0.00 of Complete::Fish (from Perl distribution Complete-Fish), released on 2014-07-02.
+
+=head1 DESCRIPTION
+
+Not yet implemented. Land grab :)
+
+=head1 SEE ALSO
+
+As far as I know, at the time of this writing, there's currently no CPAN modules
+related to fish shell, let alone fish tab completion.
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/Complete-Fish>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-Complete-Fish>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=Complete-Fish>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut

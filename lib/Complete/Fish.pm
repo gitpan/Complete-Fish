cf2eb5acf9cfd803b5f86e2b554d93c51d127bd8
perlancar <perlancar@gmail.com>
2014-11-29 11:19:55 +0000

    Import of PERLANCAR/Complete-Fish-0.01.tar.gz from CPAN.
    gitpan-cpan-distribution: Complete-Fish
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         PERLANCAR/Complete-Fish-0.01.tar.gz
    gitpan-cpan-author:       PERLANCAR
    gitpan-cpan-maturity:     released

--- lib/Complete/Fish.pm
+++ lib/Complete/Fish.pm
@@ -1,12 +1,95 @@
 package Complete::Fish;
 
-use 5.010001;
+our $DATE = '2014-11-29'; # DATE
+our $VERSION = '0.01'; # VERSION
 
-our $DATE = '2014-07-02'; # DATE
-our $VERSION = '0.00'; # VERSION
+use 5.010001;
+use strict;
+use warnings;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(
+                       format_completion
+               );
+
+require Complete::Bash;
+
+our %SPEC;
+
+# parse_cmdline is currently not needed because we invoke the 'complete' command
+# on a per-option basis and Perinci::CmdLine- or Getopt::Long::Complete-based
+# program will receive COMP_OPT/COMP_WORD instead of COMP_LINE/COMP_POINT.
+
+$SPEC{format_completion} = {
+    v => 1.1,
+    summary => 'Format completion for output (for shell)',
+    description => <<'_',
+
+fish accepts completion reply in the form of one entry per line to STDOUT.
+Description can be added to each entry, prefixed by tab character.
+
+_
+    args_as => 'array',
+    args => {
+        completion => {
+            summary => 'Completion answer structure',
+            description => <<'_',
+
+Either an array or hash, as described in `Complete`.
+
+_
+            schema=>['any*' => of => ['hash*', 'array*']],
+            req=>1,
+            pos=>0,
+        },
+    },
+    result => {
+        summary => 'Formatted string (or array, if `as` key is set to `array`)',
+        schema => ['any*' => of => ['str*', 'array*']],
+    },
+    result_naked => 1,
+};
+sub format_completion {
+    my $comp = shift;
+
+    my $as;
+    my $entries;
+
+    # we currently use Complete::Bash's rule because i haven't done a read up on
+    # how exactly fish escaping rules are.
+    if (ref($comp) eq 'HASH') {
+        $as = $comp->{as} // 'string';
+        $entries = Complete::Bash::format_completion({%$comp, as=>'array'});
+    } else {
+        $as = 'string';
+        $entries = Complete::Bash::format_completion({
+            words=>$comp, as=>'array',
+        });
+    }
+
+    # insert description
+    {
+        my $compary = ref($comp) eq 'HASH' ? $comp->{words} : $comp;
+        for (my $i=0; $i<@$compary; $i++) {
+            next unless ref($compary->[$i]) eq 'HASH';
+            my $desc = $compary->[$i]{description};
+            if (defined $desc) {
+                $desc =~ s/\R/ /g;
+                $entries->[$i] .= "\t$desc";
+            }
+        }
+    }
+
+    # turn back to string if that's what the user wants
+    if ($as eq 'string') {
+        $entries = join("", map{"$_\n"} @$entries);
+    }
+    $entries;
+}
 
 1;
-#ABSTRACT: Completion module for Fish shell
+#ABSTRACT: Completion module for fish shell
 
 __END__
 
@@ -16,20 +99,67 @@ __END__
 
 =head1 NAME
 
-Complete::Fish - Completion module for Fish shell
+Complete::Fish - Completion module for fish shell
 
 =head1 VERSION
 
-This document describes version 0.00 of Complete::Fish (from Perl distribution Complete-Fish), released on 2014-07-02.
+This document describes version 0.01 of Complete::Fish (from Perl distribution Complete-Fish), released on 2014-11-29.
 
 =head1 DESCRIPTION
 
-Not yet implemented. Land grab :)
+fish allows completion of option arguments to come from an external command,
+e.g.:
+
+ % complete -c deluser -l user -d Username -a "(cat /etc/passwd|cut -d : -f 1)"
+
+The command is supposed to return completion entries one in a separate line.
+Description for each entry can be added, prefixed with a tab character. The
+provided function C<format_completion()> accept a completion answer structure
+and format it for fish. Example:
+
+ format_completion(["a", "b", {word=>"c", description=>"Another letter"}])
+
+will result in:
+
+ a
+ b
+ c       Another letter
+
+=head1 FUNCTIONS
+
+
+=head2 format_completion($completion) -> array|str
+
+Format completion for output (for shell).
+
+fish accepts completion reply in the form of one entry per line to STDOUT.
+Description can be added to each entry, prefixed by tab character.
+
+Arguments ('*' denotes required arguments):
+
+=over 4
+
+=item * B<completion>* => I<array|hash>
+
+Completion answer structure.
+
+Either an array or hash, as described in C<Complete>.
+
+=back
+
+Return value:
+
+Formatted string (or array, if `as` key is set to `array`) (any)
+
+=head1 TODOS
 
 =head1 SEE ALSO
 
-As far as I know, at the time of this writing, there's currently no CPAN modules
-related to fish shell, let alone fish tab completion.
+L<Complete>
+
+L<Complete::Bash>
+
+Fish manual.
 
 =head1 HOMEPAGE
 
@@ -49,11 +179,11 @@ feature.
 
 =head1 AUTHOR
 
-Steven Haryanto <stevenharyanto@gmail.com>
+perlancar <perlancar@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2014 by Steven Haryanto.
+This software is copyright (c) 2014 by perlancar@cpan.org.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
